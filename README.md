e33fd5efba9caef35e4956ecadcb634841a55711
akshaydig <you@example.com>
2021-12-21 11:33:06 +0530

    Added: Program to find Maximum of Three Integers

---
+++ README.md
@@ -0,0 +1,1 @@
+#Welcome To Day13 Test Maximum Of Three
---
+++ src/com/bridgelabz/genericsMaximum/Maximum.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.genericsMaximum;
+
+public class Maximum {
+	public void maxInt(Integer first, Integer second, Integer third) {
+		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0) {
+			System.out.println("First Element is Largest");
+		} else if (second.compareTo(first) >= 0 && second.compareTo(third) >= 0) {
+			System.out.println("Second Element is Largest");
+		} else {
+			System.out.println("Third Element is Largest");
+		}
+	}
+}
