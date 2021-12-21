1ae9e80de3f9506afb7fad529629171dae6bc132
akshaydig <you@example.com>
2021-12-21 12:13:05 +0530

    Added: Program to Compare Using Generic

--- src/com/bridgelabz/genericsMaximum/Maximum.java
+++ src/com/bridgelabz/genericsMaximum/Maximum.java
@@ -1,12 +1,42 @@
 package com.bridgelabz.genericsMaximum;
 
 public class Maximum {
-	public void maxFloat(Float first,Float second,Float third) {
-		if(first.compareTo(second) >=0 && first.compareTo(third) >=0) {
+	public void maxInt(Integer first, Integer second, Integer third) {
+		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0) {
 			System.out.println("First Element is Largest");
-		}else if(second.compareTo(first) >= 0 && second.compareTo(third) >=0) {
+		} else if (second.compareTo(first) >= 0 && second.compareTo(third) >= 0) {
 			System.out.println("Second Element is Largest");
-		}else {
+		} else {
+			System.out.println("Third Element is Largest");
+		}
+	}
+
+	public void maxFloat(Float first, Float second, Float third) {
+		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0) {
+			System.out.println("First Element is Largest");
+		} else if (second.compareTo(first) >= 0 && second.compareTo(third) >= 0) {
+			System.out.println("Second Element is Largest");
+		} else {
+			System.out.println("Third Element is Largest");
+		}
+	}
+
+	public void maxString(String first, String second, String third) {
+		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0) {
+			System.out.println("First Element is Largest");
+		} else if (second.compareTo(first) >= 0 && second.compareTo(third) >= 0) {
+			System.out.println("Second Element is Largest");
+		} else {
+			System.out.println("Third Element is Largest");
+		}
+	}
+
+	public <T extends Comparable<T>> void findMax(T first, T second, T third) {
+		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0) {
+			System.out.println("First Element is Largest");
+		} else if (second.compareTo(first) >= 0 && second.compareTo(third) >= 0) {
+			System.out.println("Second Element is Largest");
+		} else {
 			System.out.println("Third Element is Largest");
 		}
 	}
--- src/com/bridgelabz/genericsMaximum/TestMaximum.java
+++ src/com/bridgelabz/genericsMaximum/TestMaximum.java
@@ -6,17 +6,55 @@ public class TestMaximum {
 	public static void main(String[] args) {
 		Maximum max = new Maximum();
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("Enter First Value: ");
-		float first = scan.nextFloat();
-		
-		System.out.print("Enter Second Value: ");
-		float second = scan.nextFloat();
-		
-		System.out.print("Enter Third Value: ");
-		float third = scan.nextFloat();
-		
-		max.maxFloat(first,second,third);
-				
+
+		while (true) {
+			System.out.print("\n1.Integer\n2.Float\n3.String\nSelect Your Choice:");
+			int choice = scan.nextInt();
+
+			switch (choice) {
+			case 1: {
+				System.out.print("Enter First Value: ");
+				Integer first = scan.nextInt();
+
+				System.out.print("Enter Second Value: ");
+				Integer second = scan.nextInt();
+
+				System.out.print("Enter Third Value: ");
+				Integer third = scan.nextInt();
+
+				max.findMax(first, second, third);
+				break;
+			}
+			case 2: {
+				System.out.print("Enter First Value: ");
+				Float first = scan.nextFloat();
+
+				System.out.print("Enter Second Value: ");
+				Float second = scan.nextFloat();
+
+				System.out.print("Enter Third Value: ");
+				Float third = scan.nextFloat();
+
+				max.findMax(first, second, third);
+				break;
+			}
+			case 3: {
+				System.out.print("Enter First Value: ");
+				String first = scan.next();
+
+				System.out.print("Enter Second Value: ");
+				String second = scan.next();
+
+				System.out.print("Enter Third Value: ");
+				String third = scan.next();
+
+				max.findMax(first, second, third);
+				break;
+			}
+
+			default:
+				System.out.println("Enter Choice Between 1 and 3");
+			}
+		}
 	}
 }
